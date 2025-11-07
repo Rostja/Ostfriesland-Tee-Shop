913572a372729c4d98918e311cff87836a22dc03
Rostja <rasto.dorcak@gmail.com>
2025-11-07 14:48:50 +0100

    New Country.java and Region.java

---
+++ src/main/java/com/ostfriesischetee/ecommerce/entity/Region.java
@@ -0,0 +1,22 @@
+package com.ostfriesischetee.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name="region")
+@Data
+public class Region {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="name")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private Country country;
+}
