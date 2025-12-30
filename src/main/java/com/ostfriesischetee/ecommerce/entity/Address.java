a4206333d68e813039d9023b6f62f22933b91122
Rostja <rasto.dorcak@gmail.com>
2025-12-30 11:19:56 +0100

    Add Address.java Customer.java

---
+++ src/main/java/com/ostfriesischetee/ecommerce/entity/Address.java
@@ -0,0 +1,32 @@
+package com.ostfriesischetee.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="address")
+@Getter
+@Setter
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "street")
+    private String street;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "region")
+    private String region;
+
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "zip_code")
+    private String zipCode;
+}
