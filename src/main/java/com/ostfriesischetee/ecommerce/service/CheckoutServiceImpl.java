a2ca8c92103685dafa35a19bda35ff09771960b5
Rostja <rasto.dorcak@gmail.com>
2026-01-13 11:03:37 +0100

    implementation of serviceImpl

--- src/main/java/com/ostfriesischetee/ecommerce/service/CheckoutServiceImpl.java
+++ src/main/java/com/ostfriesischetee/ecommerce/service/CheckoutServiceImpl.java
@@ -3,8 +3,15 @@ package com.ostfriesischetee.ecommerce.service;
 import com.ostfriesischetee.ecommerce.dao.CustomerRepository;
 import com.ostfriesischetee.ecommerce.dto.Purchase;
 import com.ostfriesischetee.ecommerce.dto.PurchaseResponse;
+import com.ostfriesischetee.ecommerce.entity.Customer;
+import com.ostfriesischetee.ecommerce.entity.Order;
+import com.ostfriesischetee.ecommerce.entity.OrderItem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Set;
+import java.util.UUID;
 
 @Service
 public class CheckoutServiceImpl implements CheckoutService{
@@ -12,11 +19,43 @@ public class CheckoutServiceImpl implements CheckoutService{
     private CustomerRepository customerRepository;
 
     @Autowired
-    private CheckoutServiceImpl(CustomerRepository customerRepository){
+    public CheckoutServiceImpl(CustomerRepository customerRepository){
         this.customerRepository = customerRepository;
     }
     @Override
+    @Transactional
     public PurchaseResponse placeOrder(Purchase purchase) {
-        return null;
+
+        // retrieve the order info from dto
+        Order order = purchase.getOrder();
+
+        // generate tracking number
+        String orderTrackingNumber = generateOrderTrackingNumber();
+        order.setOrderTrackingNumber(orderTrackingNumber);
+
+        // populate order with orderItems
+        Set<OrderItem> orderItems = purchase.getOrderItems();
+        orderItems.forEach(item -> order.add(item));
+
+        // populate order with billingAddress and shippingAddress
+        order.setBillingAddress(purchase.getBillingAddress());
+        order.setShippingAddress(purchase.getShippingAddress());
+
+        // populate customer with order
+        Customer customer = purchase.getCustomer();
+        customer.add(order);
+
+        // save to the database
+        customerRepository.save(customer);
+
+        // return a response
+
+        return new PurchaseResponse(orderTrackingNumber);
+    }
+
+    private String generateOrderTrackingNumber() {
+
+        // generate a random UUID number
+        return UUID.randomUUID().toString();
     }
 }
